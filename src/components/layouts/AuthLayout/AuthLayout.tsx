06408a3bf5b72418ccb4de3e271912906395a58a
NaafiRidho <naafiridho0505@gmail.com>
2025-07-14 19:33:00 +0700

    feat: create for register page

---
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,22 @@
+import PageHead from "@/components/commons/PageHead";
+import { ReactNode } from "react";
+
+interface PropsTypes {
+  title?: string;
+  children?: ReactNode
+  
+}
+
+const AuthLayout = (props: PropsTypes) => {
+  const { title, children } = props;
+  return (
+    <>
+      <PageHead title={title} />
+      <section className="max-w-screen-3xl 3xl:container p-6">
+        {children}
+      </section>
+    </>
+  );
+};
+
+export default AuthLayout;
---
+++ src/components/views/Register/register.tsx
@@ -0,0 +1,104 @@
+import { Button, button, Card, CardBody, Input } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
+import useRegister from "./useRegister";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+
+const Register = () => {
+  const { visiblePassword, handleVisiblePassword } = useRegister();
+  return (
+    <div className="flex w-full flex-col lg:flex-row items-center justify-center gap-10 lg:gap-20">
+      <div className="flex w-full lg:w-1/3 flex-col items-center justify-center gap-10">
+        <Image
+          src="/images/general/logo.svg"
+          alt="Logo"
+          width={180}
+          height={180}
+        ></Image>
+        <Image
+          src="/images/illustrations/login.svg"
+          alt="Login"
+          className="w-2/3 lg:w-full"
+          width={1024}
+          height={1024}
+        ></Image>
+      </div>
+      <Card>
+        <CardBody className="p-8">
+          <h2 className="text-xl font-bold text-danger-500">Created Account</h2>
+          <p className="text-small">
+            Have an Account?&nbsp;
+            <Link
+              rel="stylesheet"
+              href="/login"
+              className="font-semibold text-danger-400"
+            >
+              Login Here
+            </Link>
+          </p>
+          <form className="mt-4 flex w-80 flex-col gap-4">
+            <Input
+              type="text"
+              label="Fullname"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type="text"
+              label="Username"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type="email"
+              label="Email"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type={visiblePassword.password ? "text" : "password"}
+              label="Password"
+              variant="bordered"
+              autoComplete="off"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => handleVisiblePassword("password")}
+                >
+                  {visiblePassword.password ? (
+                    <FaEye className="pointer-events-none text-xl text-default-400"></FaEye>
+                  ) : (
+                    <FaEyeSlash className="pointer-events-none text-xl text-default-400" />
+                  )}
+                </button>
+              }
+            />
+            <Input
+              type={visiblePassword.passwordConfirmation ? "text" : "password"}
+              label="Password Confirmation"
+              variant="bordered"
+              autoComplete="off"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => handleVisiblePassword("passwordConfirmation")}
+                >
+                  {visiblePassword.passwordConfirmation ? (
+                    <FaEye className="pointer-events-none text-xl text-default-400"></FaEye>
+                  ) : (
+                    <FaEyeSlash className="pointer-events-none text-xl text-default-400" />
+                  )}
+                </button>
+              }
+            />
+            <Button color="danger" size="lg" type="submit">Register</Button>
+          </form>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Register;

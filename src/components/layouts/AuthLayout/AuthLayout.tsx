ba91c14d96623a3ee9bf0be45d57fe8611f68887
NaafiRidho <naafiridho0505@gmail.com>
2025-07-15 21:35:56 +0700

    feat: user activation

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import PageHead from "@/components/commons/PageHead";
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 interface PropsTypes {
   title?: string;
@@ -10,12 +10,12 @@ interface PropsTypes {
 const AuthLayout = (props: PropsTypes) => {
   const { title, children } = props;
   return (
-    <>
+    <Fragment>
       <PageHead title={title} />
       <section className="max-w-screen-3xl 3xl:container p-6">
         {children}
       </section>
-    </>
+    </Fragment>
   );
 };
 

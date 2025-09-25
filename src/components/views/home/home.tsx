6e9246d1115ef94056a425c5dff1d8e13c2c2713
NaafiRidho <naafiridho0505@gmail.com>
2025-09-25 14:23:41 +0700

    feat:homepage

--- src/components/views/home/home.tsx
+++ src/components/views/home/home.tsx
@@ -1,8 +1,54 @@
-const Home = ()=>{
-    return (
-        <div>
-            <div>Home</div>
-        </div>
-    )
-}
+import { Skeleton } from "@nextui-org/react";
+import HomeSlider from "./HomeSlider/HomeSlider";
+import useHome from "./useHome";
+import Image from "next/image";
+import HomeEventlist from "./HomeEventList/HomeEventList";
+import HomeCategoryList from "./HomeCategoryList/HomeCategoryList";
+
+const Home = () => {
+  const {
+    dataBanners,
+    isLoadingBanners,
+    dataFeaturedEvents,
+    isLoadingFeaturedEvents,
+    dataLatestEvents,
+    isLoadingLatestEvents,
+    dataCategories,
+    isLoadingCategories,
+  } = useHome();
+  return (
+    <div>
+      <HomeSlider
+        banners={dataBanners?.data}
+        isLoadingBanner={isLoadingBanners}
+      />
+      <HomeEventlist
+        title="Featured Event"
+        events={dataFeaturedEvents?.data}
+        isLoading={isLoadingFeaturedEvents}
+      />
+      <Skeleton
+        isLoaded={!isLoadingBanners}
+        className="mb-16 h-[20vw] w-full rounded-xl px-6 lg:px-0"
+      >
+        <Image
+          src={dataBanners && dataBanners?.data[1]?.image}
+          alt="banner"
+          className="h-[20vw] w-full rounded-xl object-cover object-center"
+          width={1920}
+          height={800}
+        />
+      </Skeleton>
+      <HomeEventlist
+        title="Latest Event"
+        events={dataLatestEvents?.data}
+        isLoading={isLoadingLatestEvents}
+      />
+      <HomeCategoryList
+        categories={dataCategories?.data}
+        isLoading={isLoadingCategories}
+      />
+    </div>
+  );
+};
 export default Home;
--- src/constants/list.constants.ts
+++ src/constants/list.constants.ts
@@ -1,13 +1,17 @@
 const LIMIT_LISTS = [
-    {label:"8", value: 8},
-    {label:"12", value: 12},
-    {label:"16", value: 16}
+    { label: "8", value: 8 },
+    { label: "12", value: 12 },
+    { label: "16", value: 16 }
 ];
 
-const LIMIT_DEFAULT =LIMIT_LISTS[0].value;
+const LIMIT_DEFAULT = LIMIT_LISTS[0].value;
 
-const PAGE_DEFAULT=1;
+const PAGE_DEFAULT = 1;
 
 const DELAY = 1000;
 
-export {LIMIT_LISTS, LIMIT_DEFAULT, PAGE_DEFAULT, DELAY}
+const LIMIT_BANNER = 5;
+const LIMIT_EVENT = 4;
+const LIMIT_CATEGORY = 8;
+
+export { LIMIT_LISTS, LIMIT_DEFAULT, PAGE_DEFAULT, DELAY, LIMIT_BANNER, LIMIT_EVENT, LIMIT_CATEGORY }
